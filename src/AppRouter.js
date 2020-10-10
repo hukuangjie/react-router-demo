3a1c3b8b997d2226e0193cb9b7fa310a477e8245
胡旷杰 <754432607@qq.com>
2020-10-10 16:08:03 +0800

    动态获取路由信息

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -5,21 +5,34 @@ import Video from "./Pages/Video"
 import Workplace from "./Pages/Workplace"
 import './index.css'
 function AppRouter() {
+    let routerConfig = [
+        { path: '/', title: '博客首页', exact: true, component: Index },
+        { path: '/video', title: '视频教程', exact: false, component: Video },
+        { path: '/workplace', title: '职场技能', exact: false, component: Workplace },
+    ]
     return (
         <Router>
             <div className="mainDiv">
                 <div className="leftNav">
                     <h3>一级导航</h3>
                     <ul>
-                        <li><Link to="/">博客首页</Link></li>
-                        <li><Link to="/video">视频教程</Link></li>
-                        <li><Link to="/workplace">职场技能</Link></li>
+                        {
+                            routerConfig.map((item, index) => {
+                                return (
+                                    <li key={index}><Link to={item.path}>{item.title}</Link></li>
+                                )
+                            })
+                        }
                     </ul>
                 </div>
                 <div className="rightMain">
-                    <Route path="/" exact component={Index}></Route>
-                    <Route path="/video" component={Video}></Route>
-                    <Route path="/workplace" component={Workplace}></Route>
+                    {
+                            routerConfig.map((item, index) => {
+                                return (
+                                    <Route key={index} path={item.path} exact={item.exact} component={item.component}></Route>
+                                )
+                            })
+                        }
                 </div>
             </div>
         </Router>
